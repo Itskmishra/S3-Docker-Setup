5aecc8f756ff3c5e40c87bcc040f4e0e0f5d3f5a
Sumit <141756495+Itskmishra@users.noreply.github.com>
2023-08-31 20:06:29 +0530

    Update s3.js

--- s3.js
+++ s3.js
@@ -1,1 +1,14 @@
+const fs = require("fs");
+const S3rver = require("s3rver");
 
+new S3rver({
+  port: 9000,
+  address: "0.0.0.0",
+  directory: "./s3",
+  configureBuckets: [
+    {
+      name: "testing-s3rver-platform",
+      configs: [fs.readFileSync("./cors.xml")],
+    },
+  ],
+}).run();
